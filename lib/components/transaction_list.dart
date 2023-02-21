aaaa29d3d674f35f3ae2c3c42e0718e5fb61fef7
Fabiano <fabiano.pereira@seventh.com.br>
2023-02-21 10:48:43 -0300

    update

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -19,7 +19,7 @@ class TransactionList extends StatelessWidget {
                   const SizedBox(height: 20),
                   Text(
                     'Nenhuma Transação Cadastrada!',
-                    style: Theme.of(context).textTheme.headline6,
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
                   const SizedBox(height: 20),
                   SizedBox(
